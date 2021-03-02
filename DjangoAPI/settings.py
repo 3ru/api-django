29c09b519d38d171c7ec554a73c2c8d353888989
Ryuyxx <ryuyxx@gmail.com>
2021-03-02 13:12:37 +0900

    fix: allowed hosts

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+   'api-django-nextjs.herokuapp.com',
+
+]
 
 
 # Application definition
